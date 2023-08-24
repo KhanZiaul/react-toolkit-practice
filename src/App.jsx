b061673eee5e99c1160baeae25d06bb4b05655b4
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-24 17:32:28 +0600

    incrementByAmount reducer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./Redux/features/counter/counterSlice";
+
 const App = () => {
+  // const count = useSelector((state) => state.counter.count)
+  const {count} = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
   return (
     <div className='h-[50vh] flex justify-center items-center'>
       <div>
         <h2 className='text-4xl font-semibold mb-4'>Increment/Decrement Counter</h2>
         <p className='text-xl font-semibold text-center'>using redux-toolkit</p>
         <div className='flex justify-center items-center mt-7 gap-5'>
-          <button className='btn font-bold'>-</button>
-          <p className='font-bold text-xl'></p>
-          <button className='btn font-bold'>+</button>
+          <button onClick={() => dispatch(decrement())} className='btn font-bold'>-</button>
+          <p className='font-bold text-xl'>{count}</p>
+          <button onClick={() => dispatch(increment())} className='btn font-bold'>+</button>
+        <button onClick={() => dispatch(incrementByAmount(5))} className='btn font-bold'>Incremet value ( 5 )</button>
         </div>
       </div>
     </div>
