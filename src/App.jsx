0700481d6b224da05216ebaa28570a1c91be1cb0
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-24 17:06:30 +0600

    App component updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-
 const App = () => {
   return (
-    <div>
-      <h2>Hellow</h2>
+    <div className='h-[50vh] flex justify-center items-center'>
+      <div>
+        <h2 className='text-4xl font-semibold mb-4'>Increment/Decrement Counter</h2>
+        <p className='text-xl font-semibold text-center'>using redux-toolkit</p>
+        <div className='flex justify-center items-center mt-7 gap-5'>
+          <button className='btn font-bold'>-</button>
+          <p className='font-bold text-xl'></p>
+          <button className='btn font-bold'>+</button>
+        </div>
+      </div>
     </div>
   );
 };
